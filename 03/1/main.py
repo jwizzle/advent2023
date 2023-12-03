72ed3239b2c1d772395db81fc28d8bd13002d351
jwizzle <info@hossel.net>
2023-12-03 16:19:23 +0100

    day3 part 1

---
+++ 03/1/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+class Number():
+
+    def __init__(self, value, row, start_pos, end_pos):
+        self.value = value
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.row = row
+        self.part = False
+
+    def update_part_status(self, matrix):
+        adjacent = []
+        rows = []
+        if self.row > 0: rows.append(self.row-1)
+        if self.row < len(matrix)-1: rows.append(self.row+1)
+
+        for row in rows:
+            for c in range(self.start_pos-1, self.end_pos+2):
+                try:
+                    if c != -1: adjacent.append(matrix[row][c])
+                except IndexError:
+                    pass
+
+        if self.start_pos > 0: adjacent.append(matrix[self.row][self.start_pos-1])
+        if self.end_pos != len(matrix[self.row])-1: adjacent.append(matrix[self.row][self.end_pos+1])
+
+        for c in adjacent:
+            if c != '.' and not is_number(c):
+                self.part = True
+                return True
+
+    def __repr__(self):
+        return f"{self.value}"
+
+
+class Schematic():
+
+    def __init__(self):
+        self.numbers = []
+        self.rows = []
+
+    def add_row(self, row):
+        self.rows.append(row)
+        number = ''
+        start_pos = 0
+        creating_number = False
+
+        for index, c in enumerate(row):
+            if not creating_number:
+                if is_number(c):
+                    number += c
+                    start_pos = index
+                    creating_number = True
+            else:
+                if is_number(c):
+                    number += c
+                else:
+                    self.numbers.append(Number(int(number), len(self.rows)-1, start_pos, index-1))
+                    number = ''
+                    creating_number = False
+
+        if creating_number:
+            self.numbers.append(Number(int(number), len(self.rows)-1, start_pos, len(row)-1))
+
+
+    def __repr__(self):
+        return str(self.rows)
+
+
+def is_number(character):
+    if character == '0':
+        return True
+    try:
+        return int(character)
+    except ValueError:
+        return False
+
+def main():
+    scheme = Schematic()
+
+    with open('input', 'r') as f:
+        for line in f.readlines():
+            scheme.add_row(line.strip())
+
+    for n in scheme.numbers:
+        n.update_part_status(scheme.rows)
+
+    print(sum([x.value for x in scheme.numbers if x.part]))
+
+
+if __name__ == '__main__':
+    main()
