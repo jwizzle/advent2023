4d92af686061553935cd475eb1f9f4e06631b39e
Jerry Wiersma <jwiersma@hostnet.nl>
2023-12-06 17:40:08 +0100

    flex on em

--- 06/2/main.py
+++ 06/2/main.py
@@ -11,25 +11,19 @@ class Race():
         self.win_scenarios = 0
 
     def check_win_scenarios(self):
-        """In what ways can we win this race?
+        """In what ways can we win this race?"""
 
-        The commented out lines are my initial solution. For reference.
-        The current code being the fastest i've come up with. For the flex.
-        There's probably some math-wizard solution i'm still missing as well.
-        """
-        self.win_scenarios = len({i for i in range(0, int(self.time_limit/2)) if i*(self.time_limit-i) > self.record})*2+1
+        previous_distance = 0
+        for i in range(0, self.time_limit):
+            distance = i*(self.time_limit-i)
 
-#        previous_distance = 0
-#        for i in range(0, self.time_limit):
-#            distance = i*(self.time_limit-i)
-#
-#            if distance < previous_distance and distance < self.record:
-#                break
-#
-#            previous_distance = distance
-#
-#            if distance > self.record:
-#                self.win_scenarios += 1
+            if distance < previous_distance and distance < self.record:
+                break
+
+            previous_distance = distance
+
+            if distance > self.record:
+                self.win_scenarios += 1
 
     def __repr__(self):
         """Represent a race."""
@@ -40,18 +34,11 @@ def main():
         text = f.read()
 
     digits = re.findall(r'[\d]+', text)
-    maxtimes = digits[0:int(len(digits)/2)]
-    records = digits[int(len(digits)/2):len(digits)]
-    races = [Race(time_limit, record) for time_limit, record in zip(maxtimes, records)]
+    maxtime = int(digits[0])
+    record = int(digits[1])
 
-    for race in races:
-        race.check_win_scenarios()
-
-    out = 1
-    for race in races:
-        out = out * race.win_scenarios
-
-    print(out)
+    # This is the flex solution. See the race class for what i used to do. This is 10x as fast tho.
+    print(len({i for i in range(0, int(maxtime/2)) if i*(maxtime-i) > record})*2+1)
 
 if __name__ == '__main__':
     main()
